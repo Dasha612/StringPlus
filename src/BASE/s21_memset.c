73af96e60930750395f8f57d0b9e1b15d3be8dd9
Elena Constantine <elenacon@sh-g7.nn.21-school.ru>
2025-07-12 16:19:19 +0300

    feat(string): implement strncat function

--- src/BASE/s21_memset.c
+++ src/BASE/s21_memset.c
@@ -1,7 +1,7 @@
 #include "../s21_string.h"
 
 
-void *memset(void *str, int c, s21_size_t n) {
+void *s21_memset(void *str, int c, s21_size_t n) {
     unsigned char *s = (unsigned char *)str;
     for (s21_size_t i = 0; i < n; i++) {
         s[i] = (unsigned char)c;
