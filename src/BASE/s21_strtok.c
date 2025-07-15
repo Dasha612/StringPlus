caee5283af507eb0d0854aa9283e42c5b115359a
Elena Constantine <elenacon@sh-g7.nn.21-school.ru>
2025-07-15 20:57:18 +0300

    feat(string): implement strtok function

--- src/BASE/s21_strtok.c
+++ src/BASE/s21_strtok.c
@@ -2,37 +2,34 @@
 
 
 char *s21_strtok(char *str, const char *delim) {
-    static char * buffer = s21_NULL;
-    char * token_start = s21_NULL;
+    static char *buffer = S21_NULL;
+    char *token_start = S21_NULL;
+    int found = 0;
+
 
     if (str != S21_NULL) {
         buffer = str;
     }
+
     while (*buffer != '\0' && s21_strchr(delim, *buffer) != S21_NULL) {
         buffer++;
     }
-    
-  
-    if (*buffer == '\0') {
-        return S21_NULL;
-    }
-    
 
-    token_start = buffer;
-    
-
-    while (*buffer != '\0' && s21_strchr(delim, *buffer) == S21_NULL) {
-        buffer++;
-    }
 
     if (*buffer != '\0') {
-        *buffer = '\0';
-        buffer++;
+        token_start = buffer;
+        found = 1;
+        
+
+        while (*buffer != '\0' && s21_strchr(delim, *buffer) == S21_NULL) {
+            buffer++;
+        }
+
+        if (*buffer != '\0') {
+            *buffer = '\0';
+            buffer++;
+        }
     }
-    
-    return token_start;
 
-
-
-
+    return found ? token_start : S21_NULL;
 }
