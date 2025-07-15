9918c98bace04071ab40956dbcc4d18169bb12ac
FenixPam <danila6464@gmail.com>
2025-07-15 16:29:19 +0300

    (create)Create s21_strtok.c file. Need tests

---
+++ unit_test/test_s21_strtok.c
@@ -0,0 +1,85 @@
+#include <string.h>
+#include <check.h>
+// char *strtok(char *str, const char *delim)
+// Сравнение strtok и s21_strtok
+/*
+START_TEST(test1_s21_strtok) {
+    // s21_code here
+    // code here
+    ck_assert_ptr_eq(result, s21_result)
+}
+END_TEST
+
+START_TEST(test2_s21_strtok) {
+    // s21_code here
+    // code here
+    ck_assert_ptr_eq(result, s21_result)
+}
+END_TEST
+
+START_TEST(test3_s21_strtok) {
+    // s21_code here
+    // code here
+    ck_assert_ptr_eq(result, s21_result)
+}
+END_TEST
+
+START_TEST(test4_s21_strtok) {
+    // s21_code here
+    // code here
+    ck_assert_ptr_eq(result, s21_result)
+}
+END_TEST
+
+START_TEST(test5_s21_strtok) {
+    // s21_code here
+    // code here
+    ck_assert_ptr_eq(result, s21_result)
+}
+END_TEST
+
+START_TEST(test6_s21_strtok) {
+    // s21_code here
+    // code here
+    ck_assert_ptr_eq(result, s21_result)
+}
+END_TEST
+
+START_TEST(test7_s21_strtok) {
+    // s21_code here
+    // code here
+    ck_assert_ptr_eq(result, s21_result)
+}
+END_TEST
+
+START_TEST(test8_s21_strtok) {
+    // s21_code here
+    // code here
+    ck_assert_ptr_eq(result, s21_result)
+}
+END_TEST
+*/
+
+Suite *s21_strtok_suite(void) {
+    Suite *s = suite_create("s21_strtok");
+    TCase *tc = tcase_create("s21_strtok_core");
+    // tcase_add_test(tc, test1_s21_strtok);
+    // tcase_add_test(tc, test2_s21_strtok);
+    // tcase_add_test(tc, test3_s21_strtok);
+    // tcase_add_test(tc, test4_s21_strtok);
+    // tcase_add_test(tc, test5_s21_strtok);
+    // tcase_add_test(tc, test6_s21_strtok);
+    // tcase_add_test(tc, test7_s21_strtok);
+    // tcase_add_test(tc, test8_s21_strtok);
+    suite_add_tcase(s, tc);
+    return s;
+}
+
+int main() {
+    int failed;
+    SRunner *runner = srunner_create(s21_strtok_suite());
+    srunner_run_all(runner, CK_NORMAL);
+    failed = srunner_ntests_failed(runner);
+    srunner_free(runner);
+    return (failed == 0) ? 0 : 1;
+}
