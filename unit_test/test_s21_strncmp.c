3afbf1eb7ddf1b4a64ccea411fc1a69b7ea68ab5
FenixPam <danila6464@gmail.com>
2025-07-21 17:36:40 +0300

    (test)Create test strncmp

--- unit_test/test_s21_strncmp.c
+++ unit_test/test_s21_strncmp.c
@@ -1,76 +1,90 @@
 #include <string.h>
 #include <check.h>
+#include "../src/s21_string.h"
 // int strncmp(const char *s1, const char *s2, size_t n)
 // Сравнение strncmp и s21_strncmp
-/*
-START_TEST(test1_s21_strncmp) {
-    // s21_code here
-    // code here
-    ck_assert_int_eq(result, s21_result)
+START_TEST(test1_s21_strncmp_equal_strings) {
+    const char str1[] = "hello";
+    const char str2[] = "hello";
+    int s21_result = s21_strncmp(str1, str2, 5);
+    int result = strncmp(str1, str2, 5);
+    ck_assert_int_eq(s21_result, result);
 }
 END_TEST
 
-START_TEST(test2_s21_strncmp) {
-    // s21_code here
-    // code here
-    ck_assert_int_eq(result, s21_result)
+START_TEST(test2_s21_strncmp_different_first_char) {
+    const char str1[] = "apple";
+    const char str2[] = "apply";
+    int s21_result = s21_strncmp(str1, str2, 5);
+    int result = strncmp(str1, str2, 5);
+    ck_assert_int_eq(s21_result, result);
 }
 END_TEST
 
-START_TEST(test3_s21_strncmp) {
-    // s21_code here
-    // code here
-    ck_assert_int_eq(result, s21_result)
+START_TEST(test3_s21_strncmp_null_pointer_in_one_string) {
+    const char str1[] = "test";
+    int s21_result = s21_strncmp(str1, NULL, 4);
+    int result = strncmp(str1, NULL, 4);
+    ck_assert_int_eq(s21_result, result);
 }
 END_TEST
 
-START_TEST(test4_s21_strncmp) {
-    // s21_code here
-    // code here
-    ck_assert_int_eq(result, s21_result)
+START_TEST(test4_s21_strncmp_embedded_null) {
+    const char str1[] = "abc\0def";
+    const char str2[] = "abc\0ghi";
+    int s21_result = s21_strncmp(str1, str2, 6);
+    int result = strncmp(str1, str2, 6);
+    ck_assert_int_eq(s21_result, result);
 }
 END_TEST
 
-START_TEST(test5_s21_strncmp) {
-    // s21_code here
-    // code here
-    ck_assert_int_eq(result, s21_result)
+START_TEST(test5_s21_strncmp_zero_length) {
+    const char str1[] = "test";
+    const char str2[] = "test123";
+    int s21_result = s21_strncmp(str1, str2, 0);
+    int result = strncmp(str1, str2, 0);
+    ck_assert_int_eq(s21_result, result);
 }
 END_TEST
 
-START_TEST(test6_s21_strncmp) {
-    // s21_code here
-    // code here
-    ck_assert_int_eq(result, s21_result)
+START_TEST(test6_s21_strncmp_same_prefix_longer_string) {
+    const char str1[] = "hello";
+    const char str2[] = "hellolonger";
+    int s21_result = s21_strncmp(str1, str2, 5);
+    int result = strncmp(str1, str2, 5);
+    ck_assert_int_eq(s21_result, result);
 }
 END_TEST
 
-START_TEST(test7_s21_strncmp) {
-    // s21_code here
-    // code here
-    ck_assert_int_eq(result, s21_result)
+START_TEST(test7_s21_strncmp_high_ascii_values) {
+    const char str1[] = "\xff\xfe";
+    const char str2[] = "\xfe\xff";
+    int s21_result = s21_strncmp(str1, str2, 2);
+    int result = strncmp(str1, str2, 2);
+    ck_assert_int_eq(s21_result, result);
 }
 END_TEST
 
-START_TEST(test8_s21_strncmp) {
-    // s21_code here
-    // code here
-    ck_assert_int_eq(result, s21_result)
+START_TEST(test8_s21_strncmp_buffer_overflow) {
+    const char str1[] = "abcdefghij";
+    const char str2[] = "abcdexxxxxx";
+    int s21_result = s21_strncmp(str1, str2, 100); // Больше, чем длина обеих строк
+    int result = strncmp(str1, str2, 100);
+    ck_assert_int_eq(s21_result, result);
 }
 END_TEST
-*/
 
 Suite *s21_strncmp_suite(void) {
     Suite *s = suite_create("s21_strncmp");
     TCase *tc = tcase_create("s21_strncmp_core");
-    // tcase_add_test(tc, test1_s21_strncmp);
-    // tcase_add_test(tc, test2_s21_strncmp);
-    // tcase_add_test(tc, test3_s21_strncmp);
-    // tcase_add_test(tc, test4_s21_strncmp);
-    // tcase_add_test(tc, test5_s21_strncmp);
-    // tcase_add_test(tc, test6_s21_strncmp);
-    // tcase_add_test(tc, test7_s21_strncmp);
-    // tcase_add_test(tc, test8_s21_strncmp);
+    tcase_add_test(tc, test1_s21_strncmp_equal_strings);
+    tcase_add_test(tc, test2_s21_strncmp_different_first_char);
+    tcase_add_test(tc, test3_s21_strncmp_null_pointer_in_one_string);
+    tcase_add_test(tc, test4_s21_strncmp_embedded_null);
+    tcase_add_test(tc, test5_s21_strncmp_zero_length);
+    tcase_add_test(tc, test6_s21_strncmp_same_prefix_longer_string);
+    tcase_add_test(tc, test7_s21_strncmp_high_ascii_values);
+    tcase_add_test(tc, test8_s21_strncmp_buffer_overflow);
     suite_add_tcase(s, tc);
     return s;
 }
