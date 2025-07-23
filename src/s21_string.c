b729e3bc5f00a89b64ffe4def8c07c8229c46ada
FenixPam <danila6464@gmail.com>
2025-07-23 15:55:23 +0300

    (fix)delete time tests

--- src/s21_string.c
+++ src/s21_string.c
@@ -29,33 +29,3 @@ s21_size_t s21_strcspn(const char *str1, const char *str2){
     return count;
 
 }
-
-
-int main() {
-    // Тестовые примеры
-    const char *test_cases[] = {
-        "Hello123", "0123456789",
-        "Hello123", "",
-        "apple", "a",
-        "banana", "xyz",
-        "test@email.com", "@.",
-        "12345", "54321",
-        NULL
-    };
-    
-    printf("%-30s | %-10s | %-10s\n", "Test Case", "s21_strcspn", "strcspn");
-    printf("---------------------------------+------------+------------\n");
-    
-    for (int i = 0; test_cases[i] != NULL; i += 2) {
-        const char *str = test_cases[i];
-        const char *reject = test_cases[i+1];
-        
-        s21_size_t s21_result = s21_strcspn(str, reject);
-        size_t std_result = strcspn(str, reject);
-        
-        printf("%-15s + %-10s | %-10lu | %-10lu\n", 
-               str, reject, s21_result, std_result);
-    }
-    
-    return 0;
-}
