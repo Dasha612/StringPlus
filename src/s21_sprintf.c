8a2e18040e8f1788b44c0362f8aab2227b73ba61
Alena_Vasilets <antonova.aliona2010@yandex.ru>
2025-07-27 20:52:54 +0300

    feat(sprintf): handle '%c' without modifiers

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -1,16 +1,36 @@
 #include "s21_sprintf.h"
 
 int s21_sprintf(char *str, const char *format, ...) {
-  int count = 0;
-  int error = 0;
-  while (!error && *format) {
+  char *str_start = str;
+  va_list args;
+  va_start(args, format);
+  while (*format) {
     if (*format != '%') {
       *str++ = *format++;
-      count++;
     } else {
-      //обработка спецификаторов
+      format++;
+      process_specificator(&format, &str, args);
     }
   }
-  str = '\0';
-  return count;
+  *str = '\0';
+  va_end(args);
+  return str - str_start;
+}
+
+void process_specificator(const char **format, char **str, va_list args) {
+  switch(**format) {
+    case 'c': write_char(str, args); break;
+    // case 'd': write_decimal(); break;
+    // case 'f': write_float(); break;
+    // case 's': write_string(); break;
+    // case 'u': write_unsined(); break;
+    // case '%': write_percent(); break;
+    default: ; // 
+  }
+  (*format)++;
+}
+
+void write_char(char **str, va_list args) {
+  char c = (char)va_arg(args, int);
+  *(*str)++ = c;
 }
--- src/s21_sprintf.h
+++ src/s21_sprintf.h
@@ -1,2 +1,8 @@
+#include <stdarg.h>
+#include <stdio.h> //temp
+
 int s21_sprintf(char *str, const char *format, ...);
+void process_specificator(const char **format, char **str, va_list args);
+void write_char(char **str, va_list args);
+
 
