76953047320f7bd91cbf286a989512b57bc8f158
Alena_Vasilets <antonova.aliona2010@yandex.ru>
2025-07-28 04:36:58 +0300

    feat(sprintf): handle '%d' without modifiers

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -20,7 +20,7 @@ int s21_sprintf(char *str, const char *format, ...) {
 void process_specificator(const char **format, char **str, va_list args) {
   switch(**format) {
     case 'c': write_char(str, args); break;
-    // case 'd': write_decimal(); break;
+    case 'd': write_decimal(str, args); break;
     // case 'f': write_float(); break;
     case 's': write_string(str, args); break;
     // case 'u': write_unsined(); break;
@@ -33,6 +33,30 @@ void process_specificator(const char **format, char **str, va_list args) {
 void write_char(char **str, va_list args) {
   char c = (char)va_arg(args, int);
   *(*str)++ = c;
+}
+
+void write_decimal(char **str, va_list args) {
+  int decimal = (int)va_arg(args, int);
+  char buf[24] = {0};
+  int i = 0;
+ 
+  if (decimal < 0) {
+    *(*str)++ = '-';
+    decimal = -decimal;
+  }
+
+  if (decimal == 0) {
+    *(*str)++ = '0';
+  }
+
+  while (decimal > 0) {
+    buf[i++] = decimal % 10 + '0';
+    decimal /= 10;
+  }
+
+  while (i > 0) {
+    *(*str)++ = buf[--i];
+  }
 }
 
 void write_string(char **str, va_list args) {
--- src/s21_sprintf.h
+++ src/s21_sprintf.h
@@ -5,6 +5,7 @@ int s21_sprintf(char *str, const char *format, ...);
 void process_specificator(const char **format, char **str, va_list args);
 void write_char(char **str, va_list args);void process_specificator(const char **format, char **str, va_list args);
 void write_string(char **str, va_list args);
+void write_decimal(char **str, va_list args);
 void write_percent(char **str);
 
 
