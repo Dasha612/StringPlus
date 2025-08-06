e734e258fd9390257448165774fb449d330d2ea3
Dalek Leygood <dalekley@ne-a4.nn.21-school.ru>
2025-08-06 10:43:18 +0300

    refactor(strncmp): remove unnecessary break

--- src/s21_strncmp.c
+++ src/s21_strncmp.c
@@ -2,12 +2,9 @@
 
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
   int res = 0;
-  while (n-- > 0) {
+  while (n-- > 0 && (*str1 || *str2)) {
     if (*str1 != *str2) {
       res = *(unsigned char *)str1 - *(unsigned char *)str2;
-      break;
-    }
-    if (*str1 == '\0') {
       break;
     }
     str1++;
